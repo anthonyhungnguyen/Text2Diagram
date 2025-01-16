000e4c66be9229dfbc02cfcba3893a68db4e1eae
hungnp <hungnp1@ghn.vn>
2025-01-15 22:39:12 -0500

    rebuild

--- backend/start.py
+++ backend/start.py
@@ -1,7 +1,5 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 cmd = [
     "gunicorn",
@@ -12,6 +10,16 @@ cmd = [
     "uvicorn.workers.UvicornWorker",
     "--bind",
     f"0.0.0.0:{os.getenv('PORT', '8000')}",
+    "--env",
+    "GEMINI_MODEL_NAME=gemini-exp-1206",
+    "--env",
+    "TEMPERATURE=1",
+    "--env",
+    "TOP_P=0.95",
+    "--env",
+    "TOP_K=64",
+    "--env",
+    "MAX_OUTPUT_TOKENS=8192",
 ]
 
 os.execvp(cmd[0], cmd)
